fcca7be0564844df3bf1e5391d63b11800c69f5c
alex hay <alexanderhay2020@u.northwestern.edu>
2020-04-27 22:36:37 -0500

    folder reorganization

--- hw2/firmware/hw2.X/nbproject/Makefile-hw2.mk
+++ hw2/firmware/hw2.X/nbproject/Makefile-hw2.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=../../../hw1/firmware/src/main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1268050508/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1268050508/main.o.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/_ext/1268050508/main.o
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=../../../hw1/firmware/src/main.c
 
 
 
@@ -107,7 +107,19 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/1268050508/main.o: ../../../hw1/firmware/src/main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/1268050508" 
+	@${RM} ${OBJECTDIR}/_ext/1268050508/main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1268050508/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1268050508/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -Werror -Wall -MMD -MF "${OBJECTDIR}/_ext/1268050508/main.o.d" -o ${OBJECTDIR}/_ext/1268050508/main.o ../../../hw1/firmware/src/main.c    -DXPRJ_hw2=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)  -mdfp=${DFP_DIR}  
+	
 else
+${OBJECTDIR}/_ext/1268050508/main.o: ../../../hw1/firmware/src/main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/1268050508" 
+	@${RM} ${OBJECTDIR}/_ext/1268050508/main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1268050508/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1268050508/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -Werror -Wall -MMD -MF "${OBJECTDIR}/_ext/1268050508/main.o.d" -o ${OBJECTDIR}/_ext/1268050508/main.o ../../../hw1/firmware/src/main.c    -DXPRJ_hw2=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)  -mdfp=${DFP_DIR}  
+	
 endif
 
 # ------------------------------------------------------------------------------------
@@ -142,3 +154,11 @@ endif
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r build/hw2
 	${RM} -r dist/hw2
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
