a289003a441846a01cc9822c3e899a98b0a8d4e0
alex hay <alexanderhay2020@u.northwestern.edu>
2020-04-27 22:33:25 -0500

    hw2 init commit

---
+++ firmware/hw2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# hw2 configuration
+CND_ARTIFACT_DIR_hw2=dist/hw2/production
+CND_ARTIFACT_NAME_hw2=hw2.X.production.hex
+CND_ARTIFACT_PATH_hw2=dist/hw2/production/hw2.X.production.hex
+CND_PACKAGE_DIR_hw2=${CND_DISTDIR}/hw2/package
+CND_PACKAGE_NAME_hw2=hw2.x.tar
+CND_PACKAGE_PATH_hw2=${CND_DISTDIR}/hw2/package/hw2.x.tar
