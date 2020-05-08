f649b6465eafcd5a7f7ef35772ad1b169b2b3bfb
alexanderhay2020 <55127263+alexanderhay2020@users.noreply.github.com>
2020-05-08 12:36:20 -0500

    hw3 submission
    Yellow LED blinks at same frequency, but not duration. Had to break while loop, otherwise it hangs.

--- hw3/firmware/src/main.c
+++ hw3/firmware/src/main.c
@@ -4,8 +4,7 @@
 
 #include<xc.h>                      // processor SFR definitions
 #include<sys/attribs.h>             // __ISR macro
-#include<math.h>
-#include "../hw3.X/spi.h"
+#include "../hw3.X/i2c_master_noint.h"
 
 // DEVCFG0
 #pragma config DEBUG = ON           // disable debugging
@@ -39,6 +38,54 @@
 #pragma config IOL1WAY = OFF        // allow multiple reconfigurations
 
 
+void initI2C(void);
+void setPin(unsigned char add, unsigned char reg, unsigned char val);  // address, register, value
+char readPin(unsigned char add,unsigned char reg);           // address, register
+
+
+void initI2C(void){
+
+    i2c_master_setup();
+    i2c_master_start();
+    i2c_master_send(0x40);        // set the address w/ write
+    i2c_master_send(0x00);          // set the register (IODIRA)
+    i2c_master_send(0x00);          // set the value    (0000 0000) for outputs
+    i2c_master_stop();              // send a stop
+    i2c_master_start();
+    i2c_master_send(0x40);        // set the address w/ write
+    i2c_master_send(0x01);          // set the register (IODIRB)
+    i2c_master_send(0xFF);          // set the value    (1111 1111) for inputs
+    i2c_master_stop();              // send a stop
+}
+
+
+void setPin(unsigned char add, unsigned char reg, unsigned char val){
+    
+    i2c_master_start();
+    i2c_master_send(add);
+    i2c_master_send(reg);
+    i2c_master_send(val);          // FF-yellow on; 00-yellow off
+    i2c_master_stop();
+}
+
+
+char readPin(unsigned char add, unsigned char reg){
+    
+    i2c_master_start();
+    i2c_master_send(add|0);         // address for writing
+    i2c_master_send(reg);           // register to read from
+    i2c_master_restart();           // restart
+    i2c_master_send(add|0x1);         // address for reading
+    
+    char recv = i2c_master_recv();  // received value
+    
+    i2c_master_ack(1);              // acknowledge bit
+    i2c_master_stop();              // stop
+    
+    return recv;
+}
+
+
 int main() {
 
     __builtin_disable_interrupts(); // disable interrupts while initializing things
@@ -58,14 +105,36 @@ int main() {
     // do your TRIS and LAT commands here
     TRISAbits.TRISA4 = 0;           // sets A4 as output
     TRISBbits.TRISB4 = 1;           // sets B4 as input
-    LATAbits.LATA4 = 0;             // sets A4 to low
-
+    LATAbits.LATA4 = 1;             // sets A4 to low; 0-off, 1-on
+    
+    initI2C();
+    
     __builtin_enable_interrupts();
 
-    
+    unsigned char slave_add = 0x40;         // (given) 0100 0000
+    unsigned char button_pin = 0x13;        // GPIOB
+    unsigned char led_pin = 0x14;           // OLATA
+    unsigned char on = 0xFF;                // 1111 1111
+    unsigned char off = 0x00;               // 0000 0000
+    unsigned char button;                   
+
     while(1) {
-        _CP0_SET_COUNT(0);      // Setting Core Timer count to 0
         
+        // Heartbeat
+        _CP0_SET_COUNT(0);      // Setting Core Timer count to 0
+        LATAbits.LATA4 = !LATAbits.LATA4;
+        while(_CP0_GET_COUNT() < 4800000){;}    // 5Hz pulse
+        
+        button = readPin(slave_add, button_pin);      // read from slave, GPIOB
+        
+        while(!button){
+            
+            setPin(slave_add, led_pin, on);         // write to slave, OLATA, state
+//            while(_CP0_GET_COUNT() < 4800000){;}    // 5Hz pulse
+
+            break;
+        }
+        
+        setPin(slave_add, led_pin, off);
     }
 }
-
